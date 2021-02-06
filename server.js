e212cad3f7d67e7b361f3992b502d113b4a59092
mcheering <62622009+mcheering@users.noreply.github.com>
2021-02-06 10:01:39 -0600

    fixed file paths

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 3040
-const mainDir = path.join(__dirname, "/public")
+const mainDir = path.join(__dirname, "./Develop/public")
 //brings in public folder files, such ass css and js so formatting works and client side functions work.  
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get("/notes", (req, res) => {
-      res.sendFile(path.join(mainDir, "notes.html"))
+      res.sendFile(path.join(mainDir, "./Develop/public/notes.html"))
 })
 
 app.get("/api/notes", (req, res) => {
-      res.sendFile(path.join(__dirname, "/db/db.json"))
+      res.sendFile(path.join(__dirname, "./Develop/db/db.json"))
 })
 
 app.get("/api/notes:id", (req, res) => {
-      let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
+      let savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"))
       res.json(savedNotes[Number(req.params.id)])
 })
 
@@ -28,18 +28,18 @@ app.get("/", (req, res) => {
 })
 
 app.post("/api/notes", (req, res) => {
-      let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
+      let savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"))
       newNote = req.body
       let uniqueID = (savedNotes.length).toString()
       newNote.id = uniqueID
       savedNotes.push(newNote)
 
-      fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes))
+      fs.writeFileSync("./Develop/db/db.json", JSON.stringify(savedNotes))
       res.json(savedNotes)
 })
 
 app.delete("/api/notes/:id", (req, res) => {
-      let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
+      let savedNotes = JSON.parse(fs.readFileSync("./Develop/db/db.json", "utf8"))
       let noteID = req.params.id
       let newID = 0
       savedNotes = savedNotes.filter(currNote => {
@@ -51,7 +51,7 @@ app.delete("/api/notes/:id", (req, res) => {
             newID++
       }
 
-      fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes))
+      fs.writeFileSync("./Develop/db/db.json", JSON.stringify(savedNotes))
       res.json(savedNotes)
 })
 
